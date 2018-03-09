d0ad87f2d47b74aad115c2265f0a1fbd73bc906d
adamurbina <adamurbina@gmail.com>
2018-03-09 21:57:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,21 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, elem){
-  newArray = [elem, ...array];
+  var newArray = [elem, ...array];
   return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, elem){
-  array = [elem, ...array]
-  return array
+  array = [elem, ...array];
+  return array;
+}
+
+function addElementToEndOfArray(array, elem){
+  var newArray = [...array, elem];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, elem){
+  array = [...array, elem];
+  return array;
 }
